1a25856c305b13f96a030612ec87fd78543d4e06
Srishti Gupta <srishti@hike.in>
2018-03-26 11:34:55 +0530

    float type check

--- api/apiutils/segmenter.py
+++ api/apiutils/segmenter.py
@@ -253,9 +253,12 @@ def video_image_segment(im):
 
 def video_processing_cv(filepath, filename, metadata):
 
-    topLeft_bg_normalized = [float(metadata['topLeft_bg_normalized_1']), float(metadata['topLeft_bg_normalized_2'])]
-    selected_bg_width_normalized = float(metadata['selected_bg_width_normalized'])
-    selected_bg_height_normalized = float(metadata['selected_bg_height_normalized'])
+    if isinstance(metadata['topLeft_bg_normalized_1'],float) == False:
+        topLeft_bg_normalized =[float(metadata['topLeft_bg_normalized_1']), float(metadata['topLeft_bg_normalized_2'])]
+        selected_bg_width_normalized = float(metadata['selected_bg_width_normalized'])
+        selected_bg_height_normalized = float(metadata['selected_bg_height_normalized'])
+
+
     bg_filename = metadata['bg_filename']
     output_video_filename = filename.rstrip(".mp4") + OUTPUT_VIDEO_FILE_EXTENSION
 
